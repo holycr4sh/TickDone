43ff207de00035af8106f3d98bd9c4a78d107018
Ahmed Kchouk <ahmedkchouk@tutamail.com>
2025-04-30 14:23:03 +0100

    Add button now open the new task view

--- app/src/main/java/com/example/todo_app/MainActivity.java
+++ app/src/main/java/com/example/todo_app/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+            startActivity(intent);
         });
     }
 }
