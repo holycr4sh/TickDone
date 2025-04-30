a9e2dff43744da907642d186ca8e3adca9408898
Ahmed Kchouk <ahmedkchouk@tutamail.com>
2025-04-30 12:25:23 +0100

    Fixed Bug
    Application is now executable.

--- app/src/main/java/com/example/todo_app/MainActivity.java
+++ app/src/main/java/com/example/todo_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.todo_app;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
